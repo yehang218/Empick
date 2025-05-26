276d2b72247dca84b74a35c85395b7079a00eb32
Morris235 <dltkdah777@gmail.com>
2025-05-26 11:59:41 +0900

    init project

---
+++ src/main/java/com/pive/empickbackend/EmpickBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pive.empickbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpickBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpickBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pive/empickbackend/EmpickBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pive.empickbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpickBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
