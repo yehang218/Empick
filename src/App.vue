47949d529794a4df6e4533af0a20fa776a274ac9
wooseok718 <dntjr123589@naver.com>
2025-05-29 20:58:05 +0900

    feature: app.vue 오류 수정

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,11 @@
 <template>
-  <button @click="show = true">모달 열기</button>
-
-  <Modal
-    v-if="show"
-    :message="dynamicMessage"
-    @confirm="handleYes"
-    @cancel="handleNo"
-  />
-
-    <v-app>
+  <v-app>
     <RouterView />
   </v-app>
-
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router';
-import { ref } from 'vue'
-import Modal from './components/Modal.vue'
-
-const show = ref(false)
-const dynamicMessage = ref('템플릿을 사용하시겠습니까?')
-
-function handleYes() {
-  console.log('예 클릭됨')
-  show.value = false
-}
-
-function handleNo() {
-  console.log('아니오 클릭됨')
-  show.value = false
-}
 </script>
+
+<style scoped></style>
