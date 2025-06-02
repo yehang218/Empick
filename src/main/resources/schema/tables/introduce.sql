ea735fc74b032bc7c863976bd66d5310e0c76d77
wooseok718 <dntjr123589@naver.com>
2025-06-02 09:49:21 +0900

    chore: 가독성을 위한 들여쓰기 수정

--- src/main/resources/schema/tables/introduce.sql
+++ src/main/resources/schema/tables/introduce.sql
@@ -7,61 +7,67 @@ DROP TABLE IF EXISTS introduce_template;
 DROP TABLE IF EXISTS introduce_template_item;
 
 -- introduce_template_item
-CREATE TABLE introduce_template_item (
-                                         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                                         title VARCHAR(255) NOT NULL,
-                                         member_id INT NOT NULL,
-                                         FOREIGN KEY (member_id) REFERENCES member(id)
+CREATE TABLE introduce_template_item
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    member_id INT NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member(id)
 );
 
 -- introduce_template
-CREATE TABLE introduce_template (
-                                    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                                    title VARCHAR(255) NOT NULL,
-                                    member_id INT NOT NULL,
-                                    introduce_template_item_id INT NOT NULL,
-                                    FOREIGN KEY (member_id) REFERENCES member(id),
-                                    FOREIGN KEY (introduce_template_item_id) REFERENCES introduce_template_item(id)
+CREATE TABLE introduce_template
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    member_id INT NOT NULL,
+    introduce_template_item_id INT NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member(id),
+    FOREIGN KEY (introduce_template_item_id) REFERENCES introduce_template_item(id)
 );
 
 -- introduce_standard_item
-CREATE TABLE introduce_standard_item (
-                                         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                                         content VARCHAR(255) NOT NULL,
-                                         member_id INT NOT NULL,
-                                         FOREIGN KEY (member_id) REFERENCES member(id)
+CREATE TABLE introduce_standard_item
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    content VARCHAR(255) NOT NULL,
+    member_id INT NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member(id)
 );
 
 -- introduce
-CREATE TABLE introduce (
-                           id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                           content VARCHAR(255) NOT NULL,
-                           member_id INT NOT NULL,
-                           introduce_template_id INT NOT NULL,
-                           FOREIGN KEY (member_id) REFERENCES member(id),
-                           FOREIGN KEY (introduce_template_id) REFERENCES introduce_template(id)
+CREATE TABLE introduce
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    content VARCHAR(255) NOT NULL,
+    member_id INT NOT NULL,
+    introduce_template_id INT NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member(id),
+    FOREIGN KEY (introduce_template_id) REFERENCES introduce_template(id)
 );
 
 -- introduce_standard
-CREATE TABLE introduce_standard (
-                                    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                                    content VARCHAR(255) NOT NULL,
-                                    member_id INT NOT NULL,
-                                    introduce_id INT NOT NULL,
-                                    introduce_standard_item_id INT NOT NULL,
-                                    FOREIGN KEY (member_id) REFERENCES member(id),
-                                    FOREIGN KEY (introduce_id) REFERENCES introduce(id),
-                                    FOREIGN KEY (introduce_standard_item_id) REFERENCES introduce_standard_item(id)
+CREATE TABLE introduce_standard
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    content VARCHAR(255) NOT NULL,
+    member_id INT NOT NULL,
+    introduce_id INT NOT NULL,
+    introduce_standard_item_id INT NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member(id),
+    FOREIGN KEY (introduce_id) REFERENCES introduce(id),
+    FOREIGN KEY (introduce_standard_item_id) REFERENCES introduce_standard_item(id)
 );
 
 -- introduce_rating_result
-CREATE TABLE introduce_rating_result (
-                                         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                                         member_id INT NOT NULL,
-                                         content LONGTEXT NOT NULL,
-                                         rating_score INT NOT NULL,
-                                         introduce_standard_id INT NOT NULL,
-                                         status TINYINT(1) NOT NULL COMMENT '확인=1, 탈락=0, 보류=2',
-                                         FOREIGN KEY (member_id) REFERENCES member(id),
-                                         FOREIGN KEY (introduce_standard_id) REFERENCES introduce_standard(id)
+CREATE TABLE introduce_rating_result
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id INT NOT NULL,
+    content LONGTEXT NOT NULL,
+    rating_score INT NOT NULL,
+    introduce_standard_id INT NOT NULL,
+    status TINYINT(1) NOT NULL COMMENT '확인=1, 탈락=0, 보류=2',
+    FOREIGN KEY (member_id) REFERENCES member(id),
+    FOREIGN KEY (introduce_standard_id) REFERENCES introduce_standard(id)
 );
