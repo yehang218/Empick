a5512e4f2a8967fd37e820cf4473cdf82dd88ac8
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2025-06-02 16:16:21 +0900

    chore: 실무 테스트 문제 폴더 구조 생성

--- src/main/java/com/piveguyz/empickbackend/employment/jobtest/command/application/controller/JobtestQuestionCommandController.java
+++ src/main/java/com/piveguyz/empickbackend/employment/jobtest/command/application/controller/JobtestQuestionCommandController.java
@@ -1,20 +1,22 @@
 package com.piveguyz.empickbackend.employment.jobtest.command.application.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // 문제 등록 관련 API
 @RestController
 @RequestMapping("/api/v1/employment/jobtest/question")
-public class QuestionCommandController {
+public class JobtestQuestionCommandController {
 
-    public QuestionCommandController() {
+    public JobtestQuestionCommandController() {
 
     }
 
     // 실무 테스트 문제 등록
+//    @PostMapping
+//    public ResponseEntity<ApiResponse<QuestionCommandDTO>> registJobtestQuestion(@RequestBody)
 
 
 
