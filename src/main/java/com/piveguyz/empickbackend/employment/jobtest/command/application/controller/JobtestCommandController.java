2cf7c95c4be14b04659c58ec894180a068e979ee
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2025-06-02 14:41:43 +0900

    feat : 기본 구조 작성중

--- src/main/java/com/piveguyz/empickbackend/employment/jobtest/command/application/controller/JobtestCommandController.java
+++ src/main/java/com/piveguyz/empickbackend/employment/jobtest/command/application/controller/JobtestCommandController.java
@@ -1,4 +1,16 @@
 package com.piveguyz.empickbackend.employment.jobtest.command.application.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/employment/jobtest")
 public class JobtestCommandController {
+
+
+    // 실무테스트 등록
+
+
+    // 실무테스트
+
 }
---
+++ src/main/java/com/piveguyz/empickbackend/employment/jobtest/command/application/controller/QuestionCommandController.java
@@ -0,0 +1,25 @@
+package com.piveguyz.empickbackend.employment.jobtest.command.application.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// 문제 등록 관련 API
+@RestController
+@RequestMapping("/api/v1/employment/jobtest/question")
+public class QuestionCommandController {
+
+    public QuestionCommandController() {
+
+    }
+
+    // 실무 테스트 문제 등록
+
+
+
+    // 실무 테스트 문제 수정
+
+
+    // 실무 테스트 문제 삭제
+}
