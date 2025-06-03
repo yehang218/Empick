017b3f88b2f5e125f89a072259ac5377e26bb5ce
HMYIEN <bsm0407@naver.com>
2025-06-03 16:31:35 +0900

    feature: 채용 요청서 Query 기능 구현 중

--- src/main/java/com/piveguyz/empickbackend/employment/recruitmentRequest/query/dto/RecruitmentRequestQueryDTO.java
+++ src/main/java/com/piveguyz/empickbackend/employment/recruitmentRequest/query/dto/RecruitmentRequestQueryDTO.java
@@ -1,4 +1,21 @@
 package com.piveguyz.empickbackend.employment.recruitmentRequest.query.dto;
 
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
 public class RecruitmentRequestQueryDTO {
+	private int id;
+	private int headcount;
+	private LocalDateTime started_at;
+	private LocalDateTime ended_at;
+	private String qualification;
+	private String preference;
+	private String responsibility;
+	private String employment_type;
+	private String work_location;
+	private LocalDateTime created_at;
+	private int member_id;
+	private int department_id;
 }
