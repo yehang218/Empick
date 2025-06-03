da00efb382413b28f3dad94e2bca3eac73323ead
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2025-06-03 17:40:45 +0900

    feat: QuestionQueryDTO 생성

--- src/main/java/com/piveguyz/empickbackend/employment/jobtests/question/query/controller/QuestionQueryController.java
+++ src/main/java/com/piveguyz/empickbackend/employment/jobtests/question/query/controller/QuestionQueryController.java
@@ -1,4 +1,15 @@
 package com.piveguyz.empickbackend.employment.jobtests.question.query.controller;
 
+
+import com.piveguyz.empickbackend.employment.jobtests.question.query.dto.QuestionQueryDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/question")
 public class QuestionQueryController {
+
+//    @GetMapping("/{questionId}")
+//    public QuestionQueryDTO
 }
--- src/main/java/com/piveguyz/empickbackend/employment/jobtests/question/query/dto/QuestionQueryDTO.java
+++ src/main/java/com/piveguyz/empickbackend/employment/jobtests/question/query/dto/QuestionQueryDTO.java
@@ -1,4 +1,47 @@
 package com.piveguyz.empickbackend.employment.jobtests.question.query.dto;
 
+import com.piveguyz.empickbackend.employment.jobtests.common.enums.JobtestDifficulty;
+import com.piveguyz.empickbackend.employment.jobtests.question.command.domain.aggregate.enums.QuestionType;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@ToString
+@Getter
+@NoArgsConstructor
 public class QuestionQueryDTO {
+    private int id;
+    private String content;
+    private String detail_content;
+    private QuestionType type;
+    private JobtestDifficulty difficulty;
+    private String answer;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    private int createdMemberId;
+    private int updatedMemberId;
+
+    @Builder
+    public QuestionQueryDTO(int id,
+                            String content,
+                            String detail_content,
+                            QuestionType type,
+                            JobtestDifficulty difficulty,
+                            String answer,
+                            LocalDateTime createdAt,
+                            LocalDateTime updatedAt,
+                            int createdMemberId,
+                            int updatedMemberId) {
+        this.id = id;
+        this.content = content;
+        this.detail_content = detail_content;
+        this.type = type;
+        this.difficulty = difficulty;
+        this.answer = answer;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.createdMemberId = createdMemberId;
+        this.updatedMemberId = updatedMemberId;
+    }
 }
