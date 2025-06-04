b0decae6ab3eb9ba09bb1772bdfab829466e4776
wooseok718 <dntjr123589@naver.com>
2025-06-05 06:27:45 +0900

    feat: 지원서 전체 조회 개발

--- src/main/java/com/piveguyz/empickbackend/employment/applicant/query/controller/ApplicationQueryController.java
+++ src/main/java/com/piveguyz/empickbackend/employment/applicant/query/controller/ApplicationQueryController.java
@@ -1,4 +1,28 @@
 package com.piveguyz.empickbackend.employment.applicant.query.controller;
 
+import com.piveguyz.empickbackend.employment.applicant.query.dto.ApplicationQueryDTO;
+import com.piveguyz.empickbackend.employment.applicant.query.service.ApplicationQueryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Tag(name = "Application", description = "지원서 조회 API")
+@RestController
+@RequestMapping("/api/v1/application")
+@RequiredArgsConstructor
 public class ApplicationQueryController {
+
+    private final ApplicationQueryService applicationQueryService;
+
+    @Operation(summary = "전체 지원서 조회", description = "지원서 전체를 조회합니다.")
+    @GetMapping
+    public ResponseEntity<List<ApplicationQueryDTO>> getAllApplication() {
+        return ResponseEntity.ok(applicationQueryService.findAllApplication());
+    }
 }
--- src/main/java/com/piveguyz/empickbackend/employment/applicant/query/dto/ApplicationQueryDTO.java
+++ src/main/java/com/piveguyz/empickbackend/employment/applicant/query/dto/ApplicationQueryDTO.java
@@ -1,5 +1,26 @@
 package com.piveguyz.empickbackend.employment.applicant.query.dto;
 
-public class ApplicationQueryDTO
-{
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApplicationQueryDTO {
+    private long id;
+    private long recruitmentId;
+    private LocalDateTime createdAt;
+    private int status; // 또는 enum으로 선언해도 좋음
+    private long applicantId;
+    private Long introduceRatingResultId; // nullable
+    private Long jobTestEvaluationResultId; // nullable
+    private Long interviewId; // nullable
+    private LocalDateTime updatedAt; // DATETIME 타입은 LocalDateTime이 적절
+    private Long updatedBy; // 수정자 member_id
+
 }
