6a2885dcf72e00ce2e9dfaa76ab6ec787c6ce34a
Morris235 <dltkdah777@gmail.com>
2025-06-05 09:12:05 +0900

    feat : MemberSignUpResponseDTO 구현

--- src/main/java/com/piveguyz/empickbackend/member/command/application/dto/MemberSignUpResponseDTO.java
+++ src/main/java/com/piveguyz/empickbackend/member/command/application/dto/MemberSignUpResponseDTO.java
@@ -1,4 +1,18 @@
 package com.piveguyz.empickbackend.member.command.application.dto;
 
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class MemberSignUpResponseDTO {
+    private int id;
+    private String email;
+    private String name;
+    private Integer employeeNumber;
+    private LocalDate createdAt;
 }
