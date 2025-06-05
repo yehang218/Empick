74f0ff4a802bab0fe6435e1fcfd725f0e8e401fe
wooseok718 <dntjr123589@naver.com>
2025-06-05 16:14:26 +0900

    ref: api 요청 메세지 추가

--- src/main/java/com/piveguyz/empickbackend/employment/applicant/query/controller/ApplicantQueryController.java
+++ src/main/java/com/piveguyz/empickbackend/employment/applicant/query/controller/ApplicantQueryController.java
@@ -1,8 +1,12 @@
 package com.piveguyz.empickbackend.employment.applicant.query.controller;
 
+import com.piveguyz.empickbackend.common.response.CustomApiResponse;
+import com.piveguyz.empickbackend.common.response.ResponseCode;
 import com.piveguyz.empickbackend.employment.applicant.query.dto.ApplicationQueryDTO;
 import com.piveguyz.empickbackend.employment.applicant.query.service.ApplicantQueryService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,8 +25,16 @@ public class ApplicantQueryController {
     private final ApplicantQueryService applicantQueryService;
 
     @Operation(summary = "지원자 전체 조회", description = "지원자 목록 전체를 조회합니다.")
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청이 성공적으로 처리되었습니다."),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청입니다."),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류입니다.")
+    })
+
     @GetMapping
-    public ResponseEntity<List<ApplicationQueryDTO>> getAllApplicant() {
-        return ResponseEntity.ok(applicantQueryService.findAllApplicant());
+    public ResponseEntity<CustomApiResponse<List<ApplicationQueryDTO>>> getAllApplicant() {
+        return ResponseEntity.status(ResponseCode.SUCCESS.getHttpStatus())
+                .body(CustomApiResponse.of(ResponseCode.SUCCESS, applicantQueryService.findAllApplicant()));
     }
 }
