02bc734ac62566e405fe0b3aaad5bcc4ab70dab0
MJay1123 <tjalswhd1@naver.com>
2025-06-05 15:00:33 +0900

    refactor : swagger 테스트 설명 추가

--- src/main/java/com/piveguyz/empickbackend/employment/interviewSheet/query/controller/InterviewSheetQueryController.java
+++ src/main/java/com/piveguyz/empickbackend/employment/interviewSheet/query/controller/InterviewSheetQueryController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Tag(name="면접 평가표 항목 Query API", description="면접 평가표 항목 조회")
+@Tag(name="면접 평가표 Query API", description="면접 평가표 조회")
 @RestController
 @RequestMapping("api/v1/employment/interviewSheet")
 public class InterviewSheetQueryController {
@@ -28,9 +28,9 @@ public class InterviewSheetQueryController {
     }
 
     @Operation(
-            summary = "면접 평가표 항목 전체 조회",
+            summary = "면접 평가표 전체 조회",
             description = """
-                    - 면접 평가표에 등록되어 있던 항목을 전부 조회합니다.
+                    - 면접 평가표를 전부 조회합니다.
                     """
     )
     @ApiResponses(value = {
@@ -46,15 +46,15 @@ public class InterviewSheetQueryController {
     }
 
     @Operation(
-            summary = "면접 평가표 항목 id로 조회",
+            summary = "면접 평가표 id로 조회",
             description = """
-                    - 면접 평가표에 등록되어 있던 항목을 id로 조회합니다.
+                    - 면접 평가표를 id로 조회합니다.
                     """
     )
     @ApiResponses(value = {
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "요청이 성공적으로 처리되었습니다."),
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "잘못된 요청입니다."),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "2530", description = "존재하지 않는 항목입니다.")
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "2520", description = "존재하지 않는 항목입니다.")
     })
     @GetMapping("/findById")
     public ResponseEntity<CustomApiResponse<InterviewSheetQueryDTO>> findById(@RequestParam("id") Integer id) {
@@ -65,9 +65,9 @@ public class InterviewSheetQueryController {
     }
 
     @Operation(
-            summary = "면접 평가표 항목 이름으로 검색",
+            summary = "면접 평가표 이름으로 검색",
             description = """
-                    - 면접 평가표에 등록되어 있던 항목을 이름으로 검색합니다.
+                    - 면접 평가표를 이름으로 검색합니다.
                     """
     )
     @ApiResponses(value = {
