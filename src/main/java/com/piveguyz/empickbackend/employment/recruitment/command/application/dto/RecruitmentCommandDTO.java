df8dbe0fc022e9c4c99c9a4bcad1d0969295dbc7
HMYIEN <bsm0407@naver.com>
2025-06-05 14:43:04 +0900

    feat: DTO 어노테이션 변경

--- src/main/java/com/piveguyz/empickbackend/employment/recruitment/command/application/dto/RecruitmentCommandDTO.java
+++ src/main/java/com/piveguyz/empickbackend/employment/recruitment/command/application/dto/RecruitmentCommandDTO.java
@@ -1,9 +1,16 @@
 package com.piveguyz.empickbackend.employment.recruitment.command.application.dto;
 
-import lombok.Data;
 import java.time.LocalDateTime;
 
-@Data
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RecruitmentCommandDTO {
 	private String title;
 	private String content;
--- src/main/java/com/piveguyz/empickbackend/employment/recruitment/query/dto/RecruitmentQueryConditionDTO.java
+++ src/main/java/com/piveguyz/empickbackend/employment/recruitment/query/dto/RecruitmentQueryConditionDTO.java
@@ -1,8 +1,14 @@
 package com.piveguyz.empickbackend.employment.recruitment.query.dto;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RecruitmentQueryConditionDTO {
 	private String keyword;
 	private Integer recruitType;
