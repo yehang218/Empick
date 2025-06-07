bde6ba0b6380222e5fd928c478bad88a6c4cbab3
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2025-06-07 22:48:59 +0900

    feat: 실무테스트 평가 기준 조회 구현

--- src/main/java/com/piveguyz/empickbackend/employment/jobtests/evaluation/query/mapper/EvaluationCriteriaMapper.java
+++ src/main/java/com/piveguyz/empickbackend/employment/jobtests/evaluation/query/mapper/EvaluationCriteriaMapper.java
@@ -1,4 +1,14 @@
 package com.piveguyz.empickbackend.employment.jobtests.evaluation.query.mapper;
 
+import com.piveguyz.empickbackend.employment.jobtests.evaluation.query.dto.EvaluationCriteriaQueryDTO;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+@Mapper
 public interface EvaluationCriteriaMapper {
+    List<EvaluationCriteriaQueryDTO> findByJobtestId(@Param("jobtestId") int jobtestId);
+
+    EvaluationCriteriaQueryDTO findById(@Param("id") int id);
 }
