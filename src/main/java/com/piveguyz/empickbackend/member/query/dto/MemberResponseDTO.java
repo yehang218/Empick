23fb8768cf41fce7d453ba1e5d9bc1f95d1cbc2f
Morris235 <dltkdah777@gmail.com>
2025-06-08 13:10:59 +0900

    refactor : 직책, 직급, 부서, 직무 표시하도록 query 수정

--- src/main/java/com/piveguyz/empickbackend/member/query/dto/MemberResponseDTO.java
+++ src/main/java/com/piveguyz/empickbackend/member/query/dto/MemberResponseDTO.java
@@ -8,16 +8,19 @@ import java.time.LocalDateTime;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class MemberResponseDTO {
     private Integer id;
     private Integer employeeNumber;
     private String name;
     private String email;
     private String phone;
+
     private String departmentName;
     private String positionName;
     private String jobName;
     private String rankName;
+
     private String pictureUrl;
     private Integer status;
     private LocalDateTime hireAt;
