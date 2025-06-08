6a2c2f99390b682571b8ed5936c468ce7d2e8726
wooseok718 <dntjr123589@naver.com>
2025-06-08 16:50:46 +0900

    feat: 자기소개서 항목별 내용 조회 개발

--- src/main/java/com/piveguyz/empickbackend/employment/applicant/query/controller/ApplicationResponseQueryController.java
+++ src/main/java/com/piveguyz/empickbackend/employment/applicant/query/controller/ApplicationResponseQueryController.java
@@ -1,4 +1,41 @@
 package com.piveguyz.empickbackend.employment.applicant.query.controller;
 
+import com.piveguyz.empickbackend.common.response.CustomApiResponse;
+import com.piveguyz.empickbackend.common.response.ResponseCode;
+import com.piveguyz.empickbackend.employment.applicant.query.dto.ApplicationResponseQueryDTO;
+import com.piveguyz.empickbackend.employment.applicant.query.service.ApplicationResponseQueryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Tag(name = "지원자 항목별 응답", description = "지원자 항목별 응답을 전체 조회합니다.")
+@RestController
+@RequestMapping("/api/v1/employment/application-response")
+@RequiredArgsConstructor
 public class ApplicationResponseQueryController {
+
+    private final ApplicationResponseQueryService applicationResponseQueryService;
+
+    @Operation(summary = "전체 지원서 평가 결과 조회", description = "지원서 평가 결과 전체 리스트를 조회합니다.")
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 조회됨"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청입니다."),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류")
+    })
+
+    @GetMapping
+    public ResponseEntity<CustomApiResponse<List<ApplicationResponseQueryDTO>>> getAllApplicationResponse() {
+        return ResponseEntity.status(ResponseCode.SUCCESS.getHttpStatus())
+                .body(CustomApiResponse.of(ResponseCode.SUCCESS, applicationResponseQueryService.findAllApplicationResponse()));
+    }
 }
+
