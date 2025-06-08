135b748fd18ec0fdf96cda0bb9081d9235707cd2
wooseok718 <dntjr123589@naver.com>
2025-06-08 15:36:24 +0900

    ref: 실무 테스트 평가 컬럼 추가

--- src/main/resources/schema/tables/applicant.sql
+++ src/main/resources/schema/tables/applicant.sql
@@ -19,11 +19,12 @@ CREATE TABLE application
 (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '지원서 번호',
     recruitment_id INT NOT NULL COMMENT '채용 공고 id',
-    created_at DATETIME NOT NULL COMMENT '작성일자' DEFAULT CURRENT_TIMESTAMP,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '작성일자',
     status TINYINT NOT NULL COMMENT '처리상태: 0(서류제외대기중), 1(서류합격), 2(실무합격), 3(1차면접합격), 4(2차면접합격), 5(최종합격), 6(불합격)',
     applicant_id INT NOT NULL COMMENT '지원자 id',
     introduce_rating_result_id INT NULL COMMENT '자기소개서 평가 결과 id',
     interview_id INT NULL COMMENT '면접 id',
+    job_test_evaluation_result_id INT NULL COMMENT '실무 테스트 평가 결과 id',
     updated_at DATETIME NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '수정시각',
     updated_by INT NULL COMMENT '수정자 (member_id)',
 
@@ -31,9 +32,10 @@ CREATE TABLE application
     FOREIGN KEY (applicant_id) REFERENCES applicant(id),
     FOREIGN KEY (introduce_rating_result_id) REFERENCES introduce_rating_result(id),
     FOREIGN KEY (interview_id) REFERENCES interview(id),
+    FOREIGN KEY (job_test_evaluation_result_id) REFERENCES job_test_evaluation_result(id),
     FOREIGN KEY (updated_by) REFERENCES member(id)
-
 );
+
 
 CREATE TABLE application_response
 (
