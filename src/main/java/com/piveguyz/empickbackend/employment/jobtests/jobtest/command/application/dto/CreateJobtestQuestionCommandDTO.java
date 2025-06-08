6809c2c75e9ff52993cddc232bafa07b0b4039b5
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2025-06-09 02:27:39 +0900

    feat: 실무테스트별 문제 등록, 수정, 삭제 구현

---
+++ src/main/java/com/piveguyz/empickbackend/employment/jobtests/jobtest/command/application/dto/CreateJobtestQuestionCommandDTO.java
@@ -0,0 +1,15 @@
+package com.piveguyz.empickbackend.employment.jobtests.jobtest.command.application.dto;
+
+import lombok.*;
+
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CreateJobtestQuestionCommandDTO {
+    private int score;
+
+    private int jobTestId;
+    private int questionId;
+}
