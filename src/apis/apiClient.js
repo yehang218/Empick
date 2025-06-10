d0be8073ece9ba69d3e15d7b8311c778bfc81ccb
Morris235 <dltkdah777@gmail.com>
2025-06-10 12:09:49 +0900

    refactor : 공통 에러 인터셉트 추가

--- src/apis/apiClient.js
+++ src/apis/apiClient.js
@@ -5,4 +5,13 @@ const api = axios.create({
     timeout: 5000,
 });
 
+api.interceptors.response.use(
+    response => response,
+    error => {
+        // 예: 토큰 만료 처리, 로깅, 공통 에러 메시지
+        console.error(error);
+        return Promise.reject(error);
+    }
+);
+
 export default api;
