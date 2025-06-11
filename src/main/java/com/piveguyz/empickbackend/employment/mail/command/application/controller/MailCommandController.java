9699ce33d20985664068c1e925abe2bc360d2268
MJay1123 <tjalswhd1@naver.com>
2025-06-11 16:23:36 +0900

    refactor : swagger 테스트 설명 추가

--- src/main/java/com/piveguyz/empickbackend/employment/mail/command/application/controller/MailCommandController.java
+++ src/main/java/com/piveguyz/empickbackend/employment/mail/command/application/controller/MailCommandController.java
@@ -40,7 +40,6 @@ public class MailCommandController {
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "2632", description = "유효하지 않은 형태의 이메일입니다."),
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "2633", description = "메일의 제목을 입력하지 않았습니다."),
     })
-
     @PostMapping("/create")
     public ResponseEntity<CustomApiResponse<MailCommandDTO>> createMail(@RequestBody MailCommandDTO mailCommandDTO) {
         MailCommandDTO createdDTO = mailCommandService.createMail(mailCommandDTO);       // DB에 메일 저장
@@ -49,6 +48,19 @@ public class MailCommandController {
                 .body(CustomApiResponse.of(result, createdDTO));
     }
 
+    @Operation(
+            summary = "안내 메일 발송",
+            description = """
+    - 안내 메일을 발송합니다.
+    """
+    )
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "요청이 성공적으로 처리되었습니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "잘못된 요청입니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "2631", description = "메일의 내용을 입력하지 않았습니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "2632", description = "유효하지 않은 형태의 이메일입니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "2633", description = "메일의 제목을 입력하지 않았습니다."),
+    })
     @PostMapping("/send")
     public ResponseEntity<CustomApiResponse<MailCommandDTO>> sendMail(@RequestBody MailCommandDTO mailCommandDTO) {
         MailCommandDTO sendedDTO = mailFacade.sendMail(mailCommandDTO);
@@ -59,6 +71,19 @@ public class MailCommandController {
 
     }
 
+    @Operation(
+            summary = "실무 테스트 일정 안내 메일 발송",
+            description = """
+    - 실무 테스트 일정 안내 메일을 발송합니다.
+    """
+    )
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "요청이 성공적으로 처리되었습니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "잘못된 요청입니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "2631", description = "메일의 내용을 입력하지 않았습니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "2632", description = "유효하지 않은 형태의 이메일입니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "2633", description = "메일의 제목을 입력하지 않았습니다."),
+    })
     @PostMapping("/send/jobtest/{id}")
     public ResponseEntity<CustomApiResponse<MailCommandDTO>> sendJobtestMail(@PathVariable("id") Integer id) {
         MailCommandDTO sendedDTO = mailFacade.sendJobtestMail(id);
@@ -67,6 +92,19 @@ public class MailCommandController {
                 .body(CustomApiResponse.of(result, sendedDTO));
     }
 
+    @Operation(
+            summary = "면접 일정 안내 메일 발송",
+            description = """
+    - 면접 일정 안내 메일을 발송합니다.
+    """
+    )
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "요청이 성공적으로 처리되었습니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "잘못된 요청입니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "2631", description = "메일의 내용을 입력하지 않았습니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "2632", description = "유효하지 않은 형태의 이메일입니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "2633", description = "메일의 제목을 입력하지 않았습니다."),
+    })
     @PostMapping("/send/interview/{id}")
     public ResponseEntity<CustomApiResponse<MailCommandDTO>> sendInterviewMail(@PathVariable("id") Integer id) {
         MailCommandDTO sendedDTO = mailFacade.sendInterviewMail(id);
