c926922b78f358278729977819c06d7e59d58b95
MJay1123 <tjalswhd1@naver.com>
2025-06-13 00:26:51 +0900

    refactor : 면접 도메인 DB 수정

--- src/main/resources/schema/tables/interview.sql
+++ src/main/resources/schema/tables/interview.sql
@@ -11,35 +11,38 @@ CREATE TABLE `interview_sheet` (
    `id`	INT	NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'id',
    `name`	VARCHAR(255)	NOT NULL COMMENT '이름',
    `is_deleted`	VARCHAR(4)	NOT NULL	DEFAULT 'N' COMMENT '삭제 여부',
-   `member_id`	INT NULL COMMENT '수정자 id',
-   `updated_at`	DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 일시',
+   `member_id`	INT NOT NULL COMMENT '수정자 id',
+   `updated_at`	DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 일시',
    FOREIGN KEY (`member_id`) REFERENCES `member`(`id`)
 )
-COMMENT = '면접 평가 기준표';
+COMMENT = '면접 평가표';
 
 CREATE TABLE `interview_criteria` (
   `id`	INT	NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'id',
+  `sheet_id` INT NOT NULL COMMENT '평가표 id',
+  `title`	VARCHAR(255)	NOT NULL COMMENT '제목',
   `content`	LONGTEXT	NOT NULL COMMENT '내용',
-  `detail_content`	LONGTEXT	NOT NULL COMMENT '상세 내용',
+    `weight` DOUBLE NOT NULL COMMENT '가중치',
   `is_deleted`	VARCHAR(4)	NOT NULL	DEFAULT 'N' COMMENT '삭제 여부',
-  `member_id`	INT	NULL COMMENT '수정자 id',
-  `updated_at`	DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 일시',
+  `member_id`	INT	NOT NULL COMMENT '수정자 id',
+  `updated_at`	DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 일시',
+    FOREIGN KEY (`sheet_id`) REFERENCES `interview_sheet`(`id`),
   FOREIGN KEY (`member_id`) REFERENCES `member`(`id`)
 )
-COMMENT = '면접 평가 기준';
+COMMENT = '면접 평가표 기준';
 
-CREATE TABLE `interview_sheet_item` (
-    `id`	INT	NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'id',
-    `sheet_id`	INT	NOT NULL COMMENT '평가표 id',
-    `criteria_id`	INT	NOT NULL COMMENT '면접 평가 기준 id',
-    `weight`	DOUBLE	NOT NULL COMMENT '가중치',
-    `member_id`	INT NULL COMMENT '수정자 id',
-    `updated_at`	DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 일시',
-    FOREIGN KEY (`sheet_id`) REFERENCES `interview_sheet`(`id`),
-    FOREIGN KEY (`criteria_id`) REFERENCES `interview_criteria`(`id`),
-    FOREIGN KEY (`member_id`) REFERENCES `member`(`id`)
-)
-COMMENT = '평가 기준표별 평가 기준';
+# CREATE TABLE `interview_sheet_item` (
+#     `id`	INT	NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'id',
+#     `sheet_id`	INT	NOT NULL COMMENT '평가표 id',
+#     `criteria_id`	INT	NOT NULL COMMENT '면접 평가 기준 id',
+#     `weight`	DOUBLE	NOT NULL COMMENT '가중치',
+#     `member_id`	INT NULL COMMENT '수정자 id',
+#     `updated_at`	DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 일시',
+#     FOREIGN KEY (`sheet_id`) REFERENCES `interview_sheet`(`id`),
+#     FOREIGN KEY (`criteria_id`) REFERENCES `interview_criteria`(`id`),
+#     FOREIGN KEY (`member_id`) REFERENCES `member`(`id`)
+# )
+# COMMENT = '평가 기준표별 평가 기준';
 
 CREATE TABLE `interview` (
      `id`	INT	NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'id',
@@ -48,7 +51,6 @@ CREATE TABLE `interview` (
      `datetime`	DATETIME	NOT NULL COMMENT '면접 일시',
      `address`	LONGTEXT	NOT NULL COMMENT '주소',
      `score`	DOUBLE	NULL COMMENT '점수',
-     `interview_review`	LONGTEXT	NULL COMMENT '면접 평가',
      FOREIGN KEY (`application_id`) REFERENCES `application`(`id`),
      FOREIGN KEY (`sheet_id`) REFERENCES `interview_sheet`(`id`)
 )
@@ -68,10 +70,10 @@ COMMENT '면접 담당자';
 CREATE TABLE `interview_score` (
    `id`	INT	NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'id',
    `interviewer_id`	INT	NOT NULL COMMENT '면접 담당자 id',
-   `item_id`	INT	NOT NULL COMMENT '평가 기준 id',
+   `criteria_id`	INT	NOT NULL COMMENT '평가 기준 id',
    `score`	INT	NULL COMMENT '점수',
    `review`	LONGTEXT    NULL COMMENT '평가',
    FOREIGN KEY (`interviewer_id`) REFERENCES `interviewer`(`id`),
-   FOREIGN KEY (`item_id`) REFERENCES `interview_sheet_item`(`id`)
+   FOREIGN KEY (`criteria_id`) REFERENCES `interview_criteria`(`id`)
 )
-COMMENT = '평가 기준별 점수';
+COMMENT = '면접 평가 점수';
