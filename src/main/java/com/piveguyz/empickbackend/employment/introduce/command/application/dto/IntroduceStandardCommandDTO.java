9215a303596100fd021956ba040797d2edb05a5a
wooseok718 <dntjr123589@naver.com>
2025-06-13 04:20:03 +0900

    feat: 자기소개서 기준표 등록/삭제 기능

--- src/main/java/com/piveguyz/empickbackend/employment/introduce/command/application/dto/IntroduceStandardCommandDTO.java
+++ src/main/java/com/piveguyz/empickbackend/employment/introduce/command/application/dto/IntroduceStandardCommandDTO.java
@@ -1,4 +1,15 @@
 package com.piveguyz.empickbackend.employment.introduce.command.application.dto;
 
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class IntroduceStandardCommandDTO {
+    private Integer id;
+    private String content;
+    private Integer memberId;
+    private Integer introduceId;
 }
