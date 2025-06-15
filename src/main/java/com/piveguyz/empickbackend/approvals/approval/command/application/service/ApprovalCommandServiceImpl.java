8a464000660883eefbf292ec90307f06f9f8fd06
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2025-06-16 02:42:32 +0900

    feat: 결재 등록 기능 구현

---
+++ src/main/java/com/piveguyz/empickbackend/approvals/approval/command/application/service/ApprovalCommandServiceImpl.java
@@ -0,0 +1,54 @@
+package com.piveguyz.empickbackend.approvals.approval.command.application.service;
+
+import com.piveguyz.empickbackend.approvals.approval.command.application.dto.ApprovalCommandResponseDTO;
+import com.piveguyz.empickbackend.approvals.approval.command.application.dto.CreateApprovalCommandDTO;
+import com.piveguyz.empickbackend.approvals.approval.command.domain.aggregate.ApprovalContentEntity;
+import com.piveguyz.empickbackend.approvals.approval.command.domain.aggregate.ApprovalEntity;
+import com.piveguyz.empickbackend.approvals.approval.command.domain.repository.ApprovalCategoryItemRepository;
+import com.piveguyz.empickbackend.approvals.approval.command.domain.repository.ApprovalContentRepository;
+import com.piveguyz.empickbackend.approvals.approval.command.domain.repository.ApprovalRepository;
+import com.piveguyz.empickbackend.orgstructure.member.command.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ApprovalCommandServiceImpl implements ApprovalCommandService {
+    private final MemberRepository memberRepository;
+    private final ApprovalRepository approvalRepository;
+    private final ApprovalCategoryItemRepository approvalCategoryItemRepository;
+    private final ApprovalContentRepository approvalContentRepository;
+
+    @Override
+    public Integer createApproval(CreateApprovalCommandDTO dto) {
+        Map<Integer, Integer> approverMap = dto.getApprovers().stream()
+                .collect(Collectors.toMap(CreateApprovalCommandDTO.ApproverDTO::getOrder, CreateApprovalCommandDTO.ApproverDTO::getMemberId));
+
+        ApprovalEntity approval = ApprovalEntity.builder()
+                .categoryId(dto.getCategoryId())
+                .writerId(dto.getWriterId())
+                .firstApproverId(approverMap.get(1))
+                .secondApproverId(approverMap.get(2))
+                .thirdApproverId(approverMap.get(3))
+                .fourthApproverId(approverMap.get(4))
+                .build();
+
+        approvalRepository.save(approval);
+
+        List<ApprovalContentEntity> contents = dto.getContents().stream()
+                .map(c -> ApprovalContentEntity.builder()
+                        .approvalId(approval.getId())
+                        .itemId(c.getItemId())
+                        .content(c.getContent())
+                        .build()
+                ).toList();
+
+        approvalContentRepository.saveAll(contents);
+
+        return approval.getId();
+    }
+}
