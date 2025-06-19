bceaed17ae6131cbc3b77bf088c443b5a0460fcd
wooseok718 <dntjr123589@naver.com>
2025-06-19 20:12:05 +0900

    fix: 파일 이름 변경

--- src/main/resources/schema/tables/introduce.sql
+++ src/main/resources/schema/tables/introduce.sql
@@ -1,12 +1,29 @@
-DROP TABLE IF EXISTS introduce_template_item;
-DROP TABLE IF EXISTS introduce_template;
-DROP TABLE IF EXISTS introduce;
-DROP TABLE IF EXISTS introduce_standard_item;
-DROP TABLE IF EXISTS introduce_standard;
-DROP TABLE IF EXISTS introduce_rating_result;
+create table introduce
+(
+    id                    int auto_increment
+        primary key,
+    content               varchar(255) not null,
+    applicant_id          int          not null,
+    introduce_template_id int          not null,
+    application_id        int          not null,
+    constraint fk_introduce_applicant
+        foreign key (applicant_id) references applicant (id),
+    constraint fk_introduce_application
+        foreign key (application_id) references application (id),
+    constraint introduce_ibfk_2
+        foreign key (introduce_template_id) references introduce_template (id)
+);
 
+create table introduce_template
+(
+    id        int auto_increment
+        primary key,
+    title     varchar(255) not null,
+    member_id int          not null,
+    constraint introduce_template_ibfk_1
+        foreign key (member_id) references member (id)
+);
 
--- introduce_template_item
 create table introduce_template_item
 (
     id                    int auto_increment
@@ -15,135 +32,6 @@ create table introduce_template_item
     member_id             int          not null,
     introduce_template_id int          null
 );
-
-create index fk_introduce_template
-    on introduce_template_item (introduce_template_id);
-
-create index member_id
-    on introduce_template_item (member_id);
-
--- introduce_template
-create table introduce_template
-(
-    id        int auto_increment
-        primary key,
-    title     varchar(255) not null,
-    member_id int          not null,
-    constraint introduce_template_ibfk_1
-        foreign key (member_id) references member (id)
-);
-
-create index member_id
-    on introduce_template (member_id);
-
-
-
-create table introduce_standard_item
-(
-    id                    int auto_increment
-        primary key,
-    content               varchar(255) not null,
-    member_id             int          not null,
-    introduce_standard_id int          not null,
-    constraint introduce_standard_item_ibfk_1
-        foreign key (member_id) references member (id)
-);
-
-create index member_id
-    on introduce_standard_item (member_id);
-
-
-
--- auto-generated definition
-create table introduce
-(
-    id                    int auto_increment
-        primary key,
-    content               varchar(255) not null,
-    member_id             int          not null,
-    introduce_template_id int          not null,
-    updated_by            int          null,
-    updated_at            datetime     null,
-    constraint introduce_ibfk_1
-        foreign key (member_id) references member (id),
-    constraint introduce_ibfk_2
-        foreign key (introduce_template_id) references introduce_template (id),
-    constraint introduce_ibfk_3
-        foreign key (updated_by) references member (id)
-);
-
-create index introduce_template_id
-    on introduce (introduce_template_id);
-
-create index member_id
-    on introduce (member_id);
-
-create index updated_by
-    on introduce (updated_by);
-
-
-
--- auto-generated definition
-create table introduce_template
-(
-    id        int auto_increment
-        primary key,
-    title     varchar(255) not null,
-    member_id int          not null,
-    constraint introduce_template_ibfk_1
-        foreign key (member_id) references member (id)
-);
-
-create index member_id
-    on introduce_template (member_id);
-
-
-
--- auto-generated definition
-create table introduce_standard_item
-(
-    id                    int auto_increment
-        primary key,
-    content               varchar(255) not null,
-    member_id             int          not null,
-    introduce_standard_id int          not null,
-    constraint introduce_standard_item_ibfk_1
-        foreign key (member_id) references member (id)
-);
-
-create index member_id
-    on introduce_standard_item (member_id);
-
-
-
--- auto-generated definition
-create table introduce_rating_result
-(
-    id                    int auto_increment
-        primary key,
-    member_id             int        not null,
-    content               longtext   not null,
-    rating_score          int        not null,
-    introduce_standard_id int        not null,
-    status                tinyint(1) not null comment '확인=1, 탈락=0, 보류=2',
-    updated_by            int        null,
-    updated_at            datetime   null,
-    constraint introduce_rating_result_ibfk_1
-        foreign key (member_id) references member (id),
-    constraint introduce_rating_result_ibfk_2
-        foreign key (introduce_standard_id) references introduce_standard (id),
-    constraint introduce_rating_result_ibfk_3
-        foreign key (updated_by) references member (id)
-);
-
-create index introduce_standard_id
-    on introduce_rating_result (introduce_standard_id);
-
-create index member_id
-    on introduce_rating_result (member_id);
-
-create index updated_by
-    on introduce_rating_result (updated_by);
 
 create table introduce_template_item_response
 (
@@ -158,10 +46,45 @@ create table introduce_template_item_response
         foreign key (introduce_template_item_id) references introduce_template_item (id)
 );
 
-create index introduce_id
-    on introduce_template_item_response (introduce_id);
+create table introduce_standard_item
+(
+    id                    int auto_increment
+        primary key,
+    content               varchar(255) not null,
+    member_id             int          not null,
+    introduce_standard_id int          not null,
+    constraint introduce_standard_item_ibfk_1
+        foreign key (member_id) references member (id),
+    foreign key (introduce_standard_id) references  introduce_standard(id)
 
-create index introduce_template_item_id
-    on introduce_template_item_response (introduce_template_item_id);
+);
 
+create table introduce_standard
+(
+    id           int auto_increment
+        primary key,
+    content      varchar(255) not null,
+    member_id    int          not null,
+    introduce_id int          not null,
+    constraint introduce_standard_ibfk_1
+        foreign key (member_id) references member (id),
+    constraint introduce_standard_ibfk_2
+        foreign key (introduce_id) references introduce (id)
+);
 
+create table introduce_rating_result
+(
+    id                    int auto_increment
+        primary key,
+    member_id             int      not null,
+    content               longtext not null,
+    rating_score          int      not null,
+    introduce_standard_id int      not null,
+    updated_by            int      null,
+    updated_at            datetime null,
+    constraint introduce_rating_result_ibfk_1
+        foreign key (member_id) references member (id),
+    constraint introduce_rating_result_ibfk_2
+        foreign key (introduce_standard_id) references introduce_standard (id),
+    constraint introduce_rating_result_ibfk_3
+        foreign key (updated_by) references member (id)
