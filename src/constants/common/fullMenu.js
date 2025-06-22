7425a6a89592979db1f08def7c4fd99eae916add
wooseok718 <dntjr123589@naver.com>
2025-06-22 16:49:12 +0900

    feat: 사이드 바에 자기소개서 기준표 버튼 추가

--- src/constants/common/fullMenu.js
+++ src/constants/common/fullMenu.js
@@ -82,7 +82,8 @@ export const fullMenu = {
                 { label: '채용 공고', path: routeMap.RecruitmentList },
                 { label: '채용 요청서', path: routeMap.RecruitmentRequestList },
 //                 { label: '채용 공고 템플릿', path: '/employment/recruitments/templates' },
-                { label: '자기소개서 템플릿', path: routeMap.IntroduceTemplateListPage }
+                { label: '자기소개서 템플릿', path: routeMap.IntroduceTemplateListPage },
+                { label: '자기소개서 기준표', path: routeMap.IntroduceStandardListPage }
             ]
         },
         {
