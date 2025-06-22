fd5d596b595808d8b66312a857c943ce57e60d92
wooseok718 <dntjr123589@naver.com>
2025-06-23 00:51:22 +0900

    ref: 자기소개서 템플릿 기준표 생성 페이지 버튼 수정

--- src/views/employment/IntroduceStandardCreatePage.vue
+++ src/views/employment/IntroduceStandardCreatePage.vue
@@ -2,7 +2,7 @@
   <v-container class="criteria-create-container">
     <div class="d-flex justify-space-between align-center mb-6">
       <h2 class="page-title">자기소개서 기준표 생성</h2>
-      <v-btn color="primary" @click="goToManage">기준표 항목 관리로 이동</v-btn>
+      <v-btn color="success" @click="submit">기준표 등록</v-btn>
     </div>
 
     <v-card class="mb-6 pa-4">
@@ -39,9 +39,8 @@
     </v-card>
 
     <div class="d-flex justify-end mt-6">
-      <v-btn color="grey" variant="outlined" class="mr-3" @click="router.back()">취소</v-btn>
-      <v-btn color="info" variant="outlined" class="mr-3" @click="goToCriteriaList">기준표 목록으로 이동</v-btn>
-      <v-btn color="success" @click="submit">기준표 등록</v-btn>
+      <v-btn color="primary" class="mr-3" @click="goToManage">기준표 항목 관리로 이동</v-btn>
+      <v-btn color="info" variant="outlined" @click="goToCriteriaList">기준표 목록으로 이동</v-btn>
     </div>
   </v-container>
 </template>
