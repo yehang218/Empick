3842e23cff238818fee0351fd97cfb184f7d6237
Morris235 <dltkdah777@gmail.com>
2025-06-25 00:28:12 +0900

    🚀 Frontend CI/CD 배포 준비
    - IntroduceStandardItemPage.vue 빈 파일 수정
    - Vue.js 빌드 성공 (6.6MB)
    - 직접 배포 방식 CI/CD 워크플로우 준비
    - S3 없는 단순 배포 구조 적용

---
+++ src/views/employment/IntroduceStandardItemPage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="introduce-standard-item-page">
+        <h1>자기소개서 표준항목 관리</h1>
+        <!-- TODO: 자기소개서 표준항목 관리 기능 구현 -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'IntroduceStandardItemPage',
+    data() {
+        return {
+            // TODO: 데이터 추가
+        }
+    }
+}
+</script>
+
+<style scoped>
+.introduce-standard-item-page {
+    padding: 20px;
+}
+</style>
