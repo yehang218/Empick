29c466f978845147e6b324598c4953adbd274efd
MJay1123 <tjalswhd1@naver.com>
2025-06-26 09:48:26 +0900

    refactor : 사원 비밀번호 데이터 생성을 위한 비밀번호 인코더 구조 수정(반복 동작)

--- src/main/java/com/piveguyz/empickbackend/common/util/PasswordEncoder.java
+++ src/main/java/com/piveguyz/empickbackend/common/util/PasswordEncoder.java
@@ -2,11 +2,20 @@ package com.piveguyz.empickbackend.common.util;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.Scanner;
+
 public class PasswordEncoder {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String rawPassword = "password1234@@";
-        String encodedPassword = encoder.encode(rawPassword);
-        System.out.println("BCrypt 암호화된 비밀번호: " + encodedPassword);
+        while(true) {
+            System.out.print("Enter password : ");
+            String rawPassword = sc.nextLine();
+            if(rawPassword.equals("quit")){
+                break;
+            }
+            String encodedPassword = encoder.encode(rawPassword);
+            System.out.println("BCrypt 암호화된 비밀번호 : " + encodedPassword);
+        }
     }
 }
